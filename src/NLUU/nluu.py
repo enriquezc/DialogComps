75ffecb0a41b25336a06fb006c8cb4e53ada53d4
Alex Mathson <mathsona@carleton.edu>
2016-11-01 12:54:32 -0500

    Updated Responses in constants.py

--- src/NLUU/nluu.py
+++ src/NLUU/nluu.py
@@ -1,26 +1,29 @@
-#import nltk
-#import pyluis
+import nltk
+import luis
 from src.Dialog_Manager import Student, Conversation
 from src.utils import constants
 import random
 
 class nLUU: 
     def __init__(self, luisurl):
-        self.luis = Luis(luisurl)
+        self.luis = luis.Luis(luisurl)
+        #Requires a local copy of atis.cfg
+        atis_grammar = nltk.data.load("atis.cfg")
+        self.parser = nltk.ChartParser(atis_grammar)
 
     def start_conversation(self):
         ''' 
             Starts conversation through command line interface. 
             To be called as first interaction with client.
         ''' 
-        student = Student()
-        conversation = Conversation()
+        #student = Student.Student()
+        #conversation = Conversation.Conversation()
         conversing = True
         our_response = "Hello. Welcome to my lair. How can I be of service?"
         while conversing:
-            client_response = input(our_response)
-            luis_analysis = get_luis(client_response)
-            tree = create_syntax_tree(client_response)
+            client_response = input(our_response + "\n")
+            luis_analysis = self.get_luis(client_response)
+            tree = self.create_syntax_tree(client_response)
             our_response_nebulous = dialog_manager.create_response(tree, luis_analysis) # tuple containing response type as first argument, and data to format for other arguments
             our_response = create_response_string(our_response_nebulous)
             if our_response == "STOP":
@@ -46,6 +49,7 @@ class nLUU:
         '''
             Creates a syntax tree from str
         '''
+        return self.parser.parse(s)
 
 
     def get_luis(self, s):
