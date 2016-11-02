eff25c87cd64e2e4690fd58ca2796f7419176532
enriquezc <tennis.sarano@gmail.com>
2016-11-02 16:31:52 -0500

    fixing task manager repository problems

---
+++ src/Task_Manager/TaskManager.py
@@ -0,0 +1,45 @@
+# Task Manager.py
+# Serves to decide what actions need to be taken next to facilitate
+# conversation for the Dialogue Manager
+
+import psycopg2
+
+
+class Course:
+    def __init__(self):
+        self.dept = None
+        self.course_num = - 1
+        self.section_num = - 1
+        self.title = ""
+        self.credits = - 1
+        self.meeting_time = []
+        self.course_description = ""
+        self.prereqs = []
+        self.instructor = ""
+
+def query_courses(course):
+    '''
+    Returns a list of course objects which share the attributes defined for the
+    course object passed as argument. Used to query courses based on multiple
+    criteria.
+    '''
+
+    # binary checking for all these conditions (512 possibilities) at this
+    # point seems super inefficient
+    # have to find a way to share some conditions
+
+
+    list_courses = []
+
+    return list_courses
+
+def query_by_string(course_description):
+    '''
+    Returns a list of course objects based on a string containing keywords
+    about the course which were not parsed into a set of specific criteria.
+    General search algorithm for courses based on a string description.
+    '''
+
+    list_courses = []
+
+    return list_courses
