627dcf85f59d89ec51bfe09fd5eacb7df465deb2
enriquezc <tennis.sarano@gmail.com>
2016-11-06 20:55:08 -0600

    finalizing course results as list

--- src/Task_Manager/TaskManager.py
+++ src/Task_Manager/TaskManager.py
@@ -3,7 +3,33 @@
 # conversation for the Dialogue Manager
 
 import psycopg2
-from src.Dialog_Manager import Course
+#from src.Dialog_Manager import Course
+
+class Course:
+    def __init__(self):
+        #name of class
+        self.name = None
+        self.id = None
+        self.prof = None
+        self.term = None
+        self.department = None
+        self.courseNum = None
+        #A score from 1-10 of how much they liked the class
+        self.sentiment = 0
+        #how confident are we in this sentiment
+        self.confidence = 0
+        self.scrunch = False
+        self.requirements_fulfilled = []
+        #Some way of storing start time, end time, and days of the week. Format undecided as of yet.
+        self.time = None
+        self.prereqs = []
+        #description from enroll
+        self.description = ""
+        #context the user gave about the class, just in case we still need it
+        self.user_description = ""
+        #Boolean. Have they taken the class yet?
+        self.taken = None
+        self.credits = None
 
 
 def connect_to_db():
@@ -22,13 +48,13 @@ def query_courses(course):
 
     conn = connect_to_db()
     #(sec_term LIKE '16%' OR sec_term LIKE '17%')
-    query_string = "SELECT * FROM COURSE WHERE "
+    query_string = "SELECT * FROM COURSE WHERE (sec_term LIKE '16%' OR sec_term LIKE '17%') AND "
 
     if course.department != None:
         query_string = query_string + "sec_subject = '" + course.department
         query_string = query_string + "' AND "
 
-    if course.name != None:
+    if course.courseNum != None:
         query_string = query_string + "sec_course_no = '" \
         + str(course.courseNum)
         query_string = query_string + "' AND "
@@ -45,9 +71,12 @@ def query_courses(course):
         result_course = Course()
         result_course.name = result[16]
         result_course.description = result[6]
-        classroom_str = classroom_str.split()
-        classroom = classroom_str[0] + " " + classroom_str[1]
-        result_course.time = classroom
+        result_course.term = result[19]
+        classroom_str = result[24]
+        if classroom_str != None:
+            classroom_str = classroom_str.split()
+            classroom = classroom_str[0] + " " + classroom_str[1]
+            result_course.time = classroom
 
         results.append(result_course)
 
@@ -72,7 +101,8 @@ def query_by_string(course_description, connection):
 if __name__ == "__main__":
     course = Course()
     course.department = "JAPN"
-    course.courseNum = "245"
+    course.courseNum = 245
     results = query_courses(course)
 
-    print(results)
+    for result in results:
+        print(str(result.name) + " " + str(result.term) + " " + str(result.description) + " " + str(result.time))
