4b9e890925036d38796bed75d941d185b5560ade
enriquezc <tennis.sarano@gmail.com>
2016-11-08 10:52:55 -0600

    Testing some stuff

--- src/Task_Manager/TaskManager.py
+++ src/Task_Manager/TaskManager.py
@@ -3,33 +3,33 @@
 # conversation for the Dialogue Manager
 
 import psycopg2
-#from src.Dialog_Manager import Course
+from src.Dialog_Manager import Course
 
-class Course:
-    def __init__(self):
-        #name of class
-        self.name = None
-        self.id = None
-        self.prof = None
-        self.term = None
-        self.department = None
-        self.courseNum = None
-        #A score from 1-10 of how much they liked the class
-        self.sentiment = 0
-        #how confident are we in this sentiment
-        self.confidence = 0
-        self.scrunch = False
-        self.requirements_fulfilled = []
-        #Some way of storing start time, end time, and days of the week. Format undecided as of yet.
-        self.time = None
-        self.prereqs = []
-        #description from enroll
-        self.description = ""
-        #context the user gave about the class, just in case we still need it
-        self.user_description = ""
-        #Boolean. Have they taken the class yet?
-        self.taken = None
-        self.credits = None
+# class Course:
+#     def __init__(self):
+#         #name of class
+#         self.name = None
+#         self.id = None
+#         self.prof = None
+#         self.term = None
+#         self.department = None
+#         self.courseNum = None
+#         #A score from 1-10 of how much they liked the class
+#         self.sentiment = 0
+#         #how confident are we in this sentiment
+#         self.confidence = 0
+#         self.scrunch = False
+#         self.requirements_fulfilled = []
+#         #Some way of storing start time, end time, and days of the week. Format undecided as of yet.
+#         self.time = None
+#         self.prereqs = []
+#         #description from enroll
+#         self.description = ""
+#         #context the user gave about the class, just in case we still need it
+#         self.user_description = ""
+#         #Boolean. Have they taken the class yet?
+#         self.taken = None
+#         self.credits = None
 
 
 def connect_to_db():
@@ -66,8 +66,10 @@ def query_courses(course):
     query_results = cur.fetchall()
 
     results = []
+    print(query_string)
 
     for result in query_results:
+        print("Getting results from query")
         result_course = Course()
         result_course.name = result[16]
         result_course.description = result[6]
@@ -101,7 +103,7 @@ def query_by_string(course_description, connection):
 if __name__ == "__main__":
     course = Course()
     course.department = "JAPN"
-    course.courseNum = 245
+    course.courseNum = 101
     results = query_courses(course)
 
     for result in results:
