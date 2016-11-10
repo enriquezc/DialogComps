b20b4c3380fcf180db7581b0a9f8a6c86dd5a9fe
Tore Banta <bantat@carleton.edu>
2016-11-09 23:11:20 -0600

    Added implementation to query course description from reason table

--- src/Task_Manager/TaskManager.py
+++ src/Task_Manager/TaskManager.py
@@ -3,33 +3,34 @@
 # conversation for the Dialogue Manager
 
 import psycopg2
-from src.Dialog_Manager import Course
+#from src.Dialog_Manager import Course
 
-# class Course:
-#     def __init__(self):
-#         #name of class
-#         self.name = None
-#         self.id = None
-#         self.prof = None
-#         self.term = None
-#         self.department = None
-#         self.courseNum = None
-#         #A score from 1-10 of how much they liked the class
-#         self.sentiment = 0
-#         #how confident are we in this sentiment
-#         self.confidence = 0
-#         self.scrunch = False
-#         self.requirements_fulfilled = []
-#         #Some way of storing start time, end time, and days of the week. Format undecided as of yet.
-#         self.time = None
-#         self.prereqs = []
-#         #description from enroll
-#         self.description = ""
-#         #context the user gave about the class, just in case we still need it
-#         self.user_description = ""
-#         #Boolean. Have they taken the class yet?
-#         self.taken = None
-#         self.credits = None
+class Course:
+    def __init__(self):
+        #name of class
+        self.name = None
+        self.id = None
+        self.prof = None
+        self.term = None
+        self.department = None
+        self.courseNum = None
+        self.term = None
+        #A score from 1-10 of how much they liked the class
+        self.sentiment = 0
+        #how confident are we in this sentiment
+        self.confidence = 0
+        self.scrunch = False
+        self.requirements_fulfilled = []
+        #Some way of storing start time, end time, and days of the week. Format undecided as of yet.
+        self.time = None
+        self.prereqs = []
+        #description from enroll
+        self.description = ""
+        #context the user gave about the class, just in case we still need it
+        self.user_description = ""
+        #Boolean. Have they taken the class yet?
+        self.taken = None
+        self.credits = None
 
 
 def connect_to_db():
@@ -48,31 +49,36 @@ def query_courses(course):
 
     conn = connect_to_db()
     #(sec_term LIKE '16%' OR sec_term LIKE '17%')
-    query_string = "SELECT * FROM COURSE WHERE (sec_term LIKE '16%' OR sec_term LIKE '17%') AND "
+    course_query = "SELECT * FROM COURSE WHERE (sec_term LIKE '16%' OR sec_term LIKE '17%') AND "
 
     if course.department != None:
-        query_string = query_string + "sec_subject = '" + course.department
-        query_string = query_string + "' AND "
+        course_query = course_query + "sec_subject = '" + course.department
+        course_query = course_query + "' AND "
 
     if course.courseNum != None:
-        query_string = query_string + "sec_course_no = '" \
+        course_query = course_query + "sec_course_no = '" \
         + str(course.courseNum)
-        query_string = query_string + "' AND "
+        course_query = course_query + "' AND "
 
-    query_string = query_string[:-5]
+    course_query = course_query[:-5]
 
     cur = conn.cursor()
-    cur.execute(query_string)
-    query_results = cur.fetchall()
+    cur.execute(course_query)
+    course_results = cur.fetchall()
+
+    print(`course_query`)
 
     results = []
-    print(query_string)
 
-    for result in query_results:
+    for result in course_results:
         print("Getting results from query")
+        print(result)
         result_course = Course()
-        result_course.name = result[16]
-        result_course.description = result[6]
+        result_course.department = result[17]
+        result_course.courseNum = result[2]
+        result_course.id = result[13]
+        result_course = result[16]
+        #result_course.comments = result[6]
         result_course.term = result[19]
         classroom_str = result[24]
         if classroom_str != None:
@@ -81,8 +87,40 @@ def query_courses(course):
             result_course.time = classroom
 
         results.append(result_course)
+        #academic_session
 
+    for result in results:
 
+        reason_query = "SELECT * FROM REASON WHERE "
+
+        if result.id != None:
+            reason_query = reason_query + "course_number = '" \
+                 + result.id
+            reason_query = reason_query + "' AND "
+
+            if result.term != None:
+                reason_query = reason_query + "academic_session = '" \
+                     + result.term
+                reason_query = reason_query + "' AND "
+
+        elif course.department != None:
+            reason_query = reason_query + "org_id = '" + course.department
+            reason_query = reason_query + "' AND "
+
+        elif course.courseNum != None:
+            reason_query = reason_query + "course_number = '" \
+            + str(course.courseNum)
+            reason_query = reason_query + "' AND "
+
+        reason_query = reason_query[:-5]
+
+        print(`reason_query`)
+
+        cur = conn.cursor()
+        cur.execute(reason_query)
+        course_results = cur.fetchone()
+
+        course.description = course_results[9]
 
     #list_courses = []
 
@@ -107,4 +145,4 @@ if __name__ == "__main__":
     results = query_courses(course)
 
     for result in results:
-        print(str(result.name) + " " + str(result.term) + " " + str(result.description) + " " + str(result.time))
+        print(str(result) + " " + str(result.term) + " " + str(result.description) + " " + str(result.time))
