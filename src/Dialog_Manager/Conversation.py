b75ab531de0b3ac123ed2b445c8ae3e077568876
samboswell <samuel.bosbetter@gmail.com>
2017-01-15 12:55:26 -0600

    decisionTree has get_next_node function now

--- src/Dialog_Manager/Conversation.py
+++ src/Dialog_Manager/Conversation.py
@@ -1,4 +1,6 @@
 import queue
+import re
+import sys
 from nltk.tree import Tree
 from src.Dialog_Manager import Student, Course, User_Query
 import luis
@@ -24,11 +26,12 @@ class Conversation:
 
 
     def __init__(self):
-        self.priority_queue = queue.PriorityQueue()
         self.student = Student.Student()
         self.last_query = 0
         self.head_node = self.NodeObject(User_Query.UserQuery(None, User_Query.QueryType.clarify),[],[])
         self.current_node = self.head_node
+        self.current_class = None
+
 
     def build_Tree(self):
         listOfEnums = [0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26, 30, 31, 32, 33, 34,
@@ -60,7 +63,7 @@ class Conversation:
 
 
         return
-class NodeObject():
+class NodeObject:
     userQuery = None
     asked = False
     answered = False
@@ -68,14 +71,16 @@ class NodeObject():
     potential_next_questions = []
 
 
-    def __init__(self, userQ, requiredQ, potentialQ):
+    def __init__(self, userQ, requiredQ, potentialQ) -> object:
         self.userQuery = userQ
         self.required_questions.extend(requiredQ)
         self.potential_next_questions.extend(potentialQ)
 
     def is_answered(self):
         return False
-   #def answer(self):
+
+    def answer(self):
+        pass
 
     def relevant_course(*args):
         pass
@@ -84,10 +89,73 @@ class NodeObject():
         pass
 
 
-   #class DecisionTree():
+class DecisionTree:
 
-        #name
-        #major
+
+
+
+
+
+    def __init__(self):
+        Conversation.build_tree()
+        current_node = None
+
+    #@params: the current node of the tree
+    #@return: the next node of the tree
+    def get_next_node(self, current_node):
+        try:
+            if current_node.answered == 1:
+                for i in range(len(current_node.required_questions)):
+                    if current_node.required_questions[i].asked or current_node.required_questions[i].answered:
+                        pass
+                    else:
+                        current_node = current_node.required_questions[i]
+
+                for i in range(len(current_node.potential_next_questions)):
+                    if current_node.potential_next_questions[i].asked or current_node.potential_next_questions[i].answered:
+                        pass
+                    else:
+                        current_node = current_node.potential_next_questions[i]
+
+                for i in range(len(current_node.required_questions)):
+                    if current_node.required_questions[i].answered:
+                        pass
+                    else:
+                        current_node = current_node.required_questions[i]
+
+                for i in range(len(current_node.required_questions)):
+                    if current_node.potential_next_questions[i].answered:
+                        pass
+                    else:
+                        current_node = current_node.potential_next_questions[i]
+
+            if current_node.answered == 0:
+                for i in range(len(current_node.potential_next_questions)):
+                    if current_node.potential_next_questions[i].asked or current_node.potential_next_questions[i].answered:
+                        pass
+                    else:
+                        current_node = current_node.potential_next_questions[i]
+
+                #have looped through and no node that is asked or answered, now loop to find one that is just not answered
+                for i in range(len(current_node.potential_next_questions)):
+                    if current_node.potential_next_questions[i].answered:
+                        pass
+                    else:
+                        current_node = current_node.potential_next_questions[i]
+
+            if current_node.userQuery > 10 and current_node.userQuery < 20:
+                return User_Query.UserQuery(Conversation.student, current_node.userQuery)
+            elif current_node.userQuery > 18:
+                return User_Query.UserQuery(Conversation.current_class, current_node.userQuery)
+            else:
+                return User_Query.UserQuery(None, current_node.userQuery)
+        except ValueError:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+
+
+
+
 
 
 
@@ -126,130 +194,35 @@ class NodeObject():
     def add_to_PriorityQueue(self, importance, user_query):
         self.priority_queue.put(importance, user_query)
 
-    # @params
-    # @return
-    def pop_priority_queue(self):
-        if self.priority_queue.empty():
-            return User_Query.UserQuery(None, User_Query.QueryType.clarify) #will fill in in a second
-        popped_userQuery = self.priority_queue.get_nowait()
-        print(popped_userQuery)
-        if popped_userQuery.type < 5:
-            return popped_userQuery
-        #for 10-17, we may format it so we only ask these questions once.
-        userQuery_course = popped_userQuery.object
-        userQuery_type = popped_userQuery.type
-        if type == 10:
-            if userQuery_course.name == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 11:
-            if userQuery_course.major == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 12:
-            return self.pop_priority_queue()
-        elif type == 13:
-            return self.pop_priority_queue()
-        elif type == 14:
-            if userQuery_course.terms_left == 12:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 15:
-            if userQuery_course.abroad == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 16:
-            return popped_userQuery
-        elif type == 17:
-            return popped_userQuery
-
-        elif type == 20 or type == 30:
-            if userQuery_course.name == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-
-        elif type == 21 or type == 31:
-            if userQuery_course.prof == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 22:
-            if userQuery_course.term == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 23 or type == 24 or type == 33:
-            if userQuery_course.sentiment == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 25:
-            if userQuery_course.scrunch == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-
-        elif type == 26 or type == 35:
-            if userQuery_course.time == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 32:
-            if userQuery_course.dept == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 34:
-            if userQuery_course.requirements == None:
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
-        elif type == 36:
-            if userQuery_course.prof == "":
-                return popped_userQuery
-            else:
-                return self.pop_priority_queue()
 
         # @params
     # @return
     def get_next_response(self, input, luisAI):
-        #self.add_to_PriorityQueue(input)
         #luis_entities = self.classify_entities(luisAI)
         #luis_intent = self.classify_intent(luisAI)
         luis_entities = luisAI.entities
         luis_intent = luisAI.intents[0]
         #entity_information = self.task_manager_information(luis_entities)
-        print(luis_intent)
+
+
         if luis_intent.intent == "ScheduleClass":
+            # if entity.type == "class":  # add more if's for different types
             course = Course.Course()
-            for entity in luis_entities:
-                if entity.type == "class":  # add more if's for different types
-                    if len(entity.entity) < 8 and entity.entity[-3:].isnumeric():
-                        course.id = entity.entity
-                        print(course.id)
-                        course.courseNum = entity.entity[-3:]
-                        course.user_description = luisAI.query
-                    else:
-                        course.name = entity.entity
-                        course.user_description = luisAI.query
-                if entity.type == "personname":
-                    course.prof = entity.entity
-                if entity.type == "time":  # time object is a list of lists, first is M-F, second is len 2,
-                    pass  # with start/end time that day?
-                    # want a parse tree / relation extraction because we do not know
-                    # whether it is during, before, or after without context.
-                if entity.type == "department":
-                    course.department = entity.entity
+            course_name = re.search("([A-Za-z]{2,4}) ?(\d{3})", input)
+
+            course.id = course_name.group(0)
+            course.courseNum = course_name.group(2)
+            course.department = course_name.group(1)
+            course.user_description = luisAI.query
+
             tm_courses = self.task_manager_information(course)
-            self.student.potential_courses = tm_courses
-            course = tm_courses
-            self.student.current_classes.append(course)
-            self.add_to_PriorityQueue(1, User_Query.UserQuery(course, User_Query.QueryType.schedule_class_res))
+            if tm_courses == None:
+                return User_Query.UserQuery(None, User_Query.QueryType.clarify)
+            else:
+                self.student.current_classes.append(tm_courses)
+                #self.student.total_credits += tm_courses.credits
+                #if self.student.total_credits < 12:
+                return User_Query.UserQuery(tm_courses, User_Query.QueryType.schedule_class_res)
 
         elif luis_intent.intent == "ClassSentiment":
             course = Course.Course()
@@ -286,8 +259,7 @@ class NodeObject():
             self.add_to_PriorityQueue(1, User_Query.UserQuery(course, User_Query.QueryType.new_class_description))
 
         elif luis_intent.intent == "WelcomeResponse":
-            #print('adding welcome')
-            self.add_to_PriorityQueue((1, User_Query.UserQuery(None, User_Query.QueryType.welcome)))
+
 
         elif luis_intent.intent == "ScheduleRequest":
             course = Course.Course()
@@ -320,13 +292,10 @@ class NodeObject():
                     self.student.current_classes.append(self.student.current_classes(course))
                 else:
                     pass
-            self.add_to_PriorityQueue(1, User_Query.UserQuery(course, User_Query.QueryType.schedule_class_res))
-            #need to add schedule request user query
 
         elif luis_intent.intent == "ClassDescriptionResponse":
             course = Course.Course()
             self.task_manager_information(course)
-            self.add_to_PriorityQueue(1, User_Query.UserQuery(course, User_Query.QueryType.new_class_description))
 
         elif luis_intent.intent == "ClassProfessorRequest":
             course = Course.Course()
@@ -408,11 +377,97 @@ class NodeObject():
 
         #else statement will ask for more information
         else:
-            print("in else")
-            self.add_to_PriorityQueue(1, User_Query.UserQuery(self.student, User_Query.QueryType.clarify))
-        #print(luis_intent)
         popped_query = self.pop_priority_queue()
         return popped_query
+
+    # @params
+    # @return
+    def pop_priority_queue(self):
+        if self.priority_queue.empty():
+            return User_Query.UserQuery(None, User_Query.QueryType.clarify)  # will fill in in a second
+        popped_userQuery = self.priority_queue.get_nowait()
+        print(popped_userQuery)
+        if popped_userQuery.type < 5:
+            return popped_userQuery
+        # for 10-17, we may format it so we only ask these questions once.
+        userQuery_course = popped_userQuery.object
+        userQuery_type = popped_userQuery.type
+        if type == 10:
+            if userQuery_course.name == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 11:
+            if userQuery_course.major == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 12:
+            return self.pop_priority_queue()
+        elif type == 13:
+            return self.pop_priority_queue()
+        elif type == 14:
+            if userQuery_course.terms_left == 12:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 15:
+            if userQuery_course.abroad == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 16:
+            return popped_userQuery
+        elif type == 17:
+            return popped_userQuery
+
+        elif type == 20 or type == 30:
+            if userQuery_course.name == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+
+        elif type == 21 or type == 31:
+            if userQuery_course.prof == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 22:
+            if userQuery_course.term == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 23 or type == 24 or type == 33:
+            if userQuery_course.sentiment == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 25:
+            if userQuery_course.scrunch == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+
+        elif type == 26 or type == 35:
+            if userQuery_course.time == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 32:
+            if userQuery_course.dept == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 34:
+            if userQuery_course.requirements == None:
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
+        elif type == 36:
+            if userQuery_course.prof == "":
+                return popped_userQuery
+            else:
+                return self.pop_priority_queue()
 
     # @params
     # @return
