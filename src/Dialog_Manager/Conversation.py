1688d6d46d6a7289320389f79784f901c8293cd0
Alex Mathson <mathsona@carleton.edu>
2017-02-22 22:53:05 -0600

    Working on handling major responses

--- src/Dialog_Manager/Conversation.py
+++ src/Dialog_Manager/Conversation.py
@@ -159,7 +159,8 @@ class Conversation:
                         return [User_Query.UserQuery(self.student_profile, User_Query.QueryType.clarify)]
                     self.call_debug_print(tm_major)
                     updated = True
-                    self.student_profile.major.add(tm_major)
+                    if len(self.student_profile.major) <= 2 and not tm_major is None:
+                        self.student_profile.major.add(tm_major)
         if not updated:
             major_list = self.getDepartmentStringFromLuis(input, luisAI, luis_intent, luis_entities)
             self.call_debug_print("major: " + str(major_list))
@@ -167,9 +168,11 @@ class Conversation:
 
                 if type(major) == type([]):
                     for m in major:
-                        self.student_profile.major.add(m)
+                        if len(self.student_profile.major) <= 2 and not m is None:
+                            self.student_profile.major.add(m)
                 else:
-                    self.student_profile.major.add(major)
+                    if len(self.student_profile.major) <= 2 and not major is None:
+                        self.student_profile.major.add(major)
         return [User_Query.UserQuery(self.student_profile, User_Query.QueryType.student_info_major_res),
                 self.decision_tree.get_next_node()]
 
