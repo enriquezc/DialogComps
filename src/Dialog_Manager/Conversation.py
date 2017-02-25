69edbe65cea773b07cf2d303b4d4c648a587ed73
Charlie <enriquezc@carleton.edu>
2017-02-25 14:42:57 -0600

    fixing a length of a list bug

--- src/Dialog_Manager/Conversation.py
+++ src/Dialog_Manager/Conversation.py
@@ -485,7 +485,7 @@ class Conversation:
             self.call_debug_print("hey")
             return [User_Query.UserQuery(self.student_profile, User_Query.QueryType.schedule_class_res), User_Query.UserQuery(self.student_profile, self.decision_tree.current_node.userQuery)]
 
-        
+
         tm_courses = self.getCoursesFromLuis(input, luisAI, luis_intent, luis_entities)
         if not tm_courses is None and len(tm_courses) > 0:  # We got returned a list
             for tm_course in tm_courses:
@@ -935,7 +935,7 @@ class Conversation:
             '''class_match = TaskManager.query_by_distribution(tm_distro, None, list(self.student_profile.interests),
                                                             list(self.student_profile.major))'''
         if len(class_match) > 0:
-            if len(class_match > 5):
+            if len(class_match) > 5:
                 class_match = class_match[0:5]
             return class_match
         else:
